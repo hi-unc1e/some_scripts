62c2ec24a1f92015829326c256bc1e60588a462b
hi-unc1e <unc1e.com@pm.me>
2020-11-26 02:49:28 +0800

    ADD Gevent event 异步通信代码

---
+++ Python_MultiThread_Practices/gevent/Async_Gevent.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+# Author : unc1e, unc1e.com@pm.me
+# Data : 2020/11/26 2:43
+import gevent
+from gevent.event import AsyncResult
+a = AsyncResult()
+
+def setter():
+    """
+    After 3 seconds set the result of a.
+    """
+    gevent.sleep(3)
+    a.set('Hello!')
+
+def waiter():
+    """
+    After 3 seconds the get call will unblock after the setter
+    puts a value into the AsyncResult.
+    """
+    print(a.get())
+
+gevent.joinall([
+    gevent.spawn(setter),
+    gevent.spawn(waiter),
+])
